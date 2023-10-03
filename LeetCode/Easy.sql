596bfc40c9f0a891ddda80969791a725a9d39fe3
vsupapo <60374437+vsupapo@users.noreply.github.com>
2023-10-02 18:24:55 -0700

    Update and rename Easy to Easy.sql

---
+++ LeetCode/Easy.sql
@@ -0,0 +1,37 @@
+/* A collection of Leetcode Solutions
+   Difficulty: EASY
+*/
+
+/* Write a solution to report the first name, last name, city, and state of each person in the Person table.
+   If the address of a personId is not present in the Address table, report null instead.
+   Return the result table in any order.
+
+   Table: Person
+   +-------------+---------+
+   | Column Name | Type    |
+   +-------------+---------+
+   | personId    | int     |
+   | lastName    | varchar |
+   | firstName   | varchar |
+   +-------------+---------+
+   personId is the primary key (column with unique values) for this table.
+   This table contains information about the ID of some persons and their first and last names.
+
+   Table: Address
+   +-------------+---------+
+   | Column Name | Type    |
+   +-------------+---------+
+   | addressId   | int     |
+   | personId    | int     |
+   | city        | varchar |
+   | state       | varchar |
+   +-------------+---------+
+   addressId is the primary key (column with unique values) for this table.
+   Each row of this table contains information about the city and state of one person with ID = PersonId.
+*/
+SELECT person.firstName, person.lastName, address.city, address.state
+FROM Person person
+LEFT JOIN Address address
+ON person.personId = address.personId
+
+
